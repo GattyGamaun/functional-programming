5bb5a231e268b054b34f7087300e057bcac95c03
Siarhei Kandratsiuk <siarhei_kandratsiuk@epam.com>
2020-07-08 17:19:42 +0300

    feat: make form working

--- src/Update.js
+++ src/Update.js
@@ -1,21 +1,73 @@
 const MESSAGES = {
   SHOW_FORM: 'SHOW_FORM',
-}
+  MEAL_INPUT: 'SAVE_MEAL',
+  CALORIES_INPUT: 'SAVE_CALORIES',
+  SAVE_FORM: 'SAVE_FORM',
+};
 
 export function showFormMsg(showForm) {
   return {
     type: MESSAGES.SHOW_FORM,
     showForm: showForm,
   }
-}
+};
+
+export function saveMealMsg(description) {
+  return {
+    type: MESSAGES.MEAL_INPUT,
+    description,
+  }
+};
+
+export function saveCaloriesMsg(calories) {
+  return {
+    type: MESSAGES.CALORIES_INPUT,
+    calories,
+  }
+};
+
+export const saveFormMsg = { type: MESSAGES.SAVE_FORM }
 
 function update(action, model) {
   switch (action.type) {
     case MESSAGES.SHOW_FORM:
       const { showForm } = action;
-      return {...model, showForm };
+      return {
+        ...model,
+        showForm,
+        description: '',
+        calories: 0,
+      };
+    case MESSAGES.MEAL_INPUT:
+      const { description } = action;
+      return {
+        ...model,
+        description: description,
+      }
+    case MESSAGES.CALORIES_INPUT:
+      const { calories } = action;
+      return {
+        ...model,
+        calories: parseInt(calories),
+      }
+    case MESSAGES.SAVE_FORM:
+      return addMeal(action, model);
   }
   return model;
+}
+
+function addMeal(action, model) {
+  const { nextId, description, calories } = model;
+  const meal = { id: nextId, description, calories };
+  const meals = [...model.meals, meal];
+
+  return {
+    ...model,
+    nextId: nextId + 1,
+    description: '',
+    calories: 0,
+    meals: meals
+  }
 }
 
 export default update;
--- src/View.js
+++ src/View.js
@@ -1,41 +1,61 @@
-import { showFormMsg } from './Update';
+import { showFormMsg, saveMealMsg, saveCaloriesMsg, saveFormMsg } from './Update';
 
 function makeButton(text) {
   const button = document.createElement('button');
   button.innerText = text;
-  button.className = 'm5'
+  button.className = 'm5';
 
   return button;
 }
 
-function makeInput(text, placeholder) {
+function makeInput(text, placeholder, className, onInput) {
   const wrapper = document.createElement('div');
   const label = document.createElement('label');
   const input = document.createElement('input');
   wrapper.className = 'm5';
   label.className = 'm-r2';
   label.innerText = text;
+  input.classList = className;
   input.placeholder = placeholder;
+  input.type = 'text';
   input.style.width = 300 + 'px';
+  input.onblur = onInput;
+
   wrapper.append(label, input);
 
   return wrapper;
 }
 
-function formView(dispatch, model) {
+function viewForm(dispatch, model) {
   let { description, calories, showForm } = model;
+
+  function onSubmit(e) {
+    e.preventDefault();
+    dispatch(saveFormMsg);
+  }
+
   const addBtn = makeButton('Add meal');
   const cancelBtn = makeButton('Cancel');
+  const saveBtn = makeButton('Save');
+  const mealInput = makeInput('Meal', description, 'meal-input',
+          e => dispatch(saveMealMsg(e.target.value)));
+  const caloriesInput = makeInput('Calories', calories || '', 'calories input',
+      e => dispatch(saveCaloriesMsg(e.target.value)));
+
+  mealInput.className = 'meal-input';
+  caloriesInput.className = 'calories-input';
+
   const form = document.createElement('form');
 
   form.append(
-      makeInput('Meal', description),
-      makeInput('Calories', calories || ''),
+      mealInput,
+      caloriesInput,
       cancelBtn,
-      makeButton('Save'));
+      saveBtn);
 
   addBtn.addEventListener('click', () => dispatch(showFormMsg(true)));
   cancelBtn.addEventListener('click', () => dispatch(showFormMsg(false)));
+  saveBtn.addEventListener('click', onSubmit);
 
   if (showForm) {
     return form;
@@ -51,7 +71,7 @@ function view(dispatch, model) {
   listField.innerText = JSON.stringify(model, null, 2);
   listField.className = 'list-field';
 
-  wrapper.append(formView(dispatch, model), listField);
+  wrapper.append(viewForm(dispatch, model), listField);
 
   return wrapper;
 }
